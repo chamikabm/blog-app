032072a946482755f7a48360edf0d67591dcd173
chamikab <kasun.chamika@gmail.com>
2018-08-12 12:31:11 +0530

    Axios interceptors function added.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,6 +11,7 @@ class Blog extends Component {
   state = {
     posts : [],
     selectedPostId: null,
+    error: false,
   };
 
   componentDidMount () {
@@ -26,6 +27,9 @@ class Blog extends Component {
         });
         this.setState({posts: updatedPosts});
       })
+      .catch(error => {
+        this.setState({error: true});
+      });
   }
 
   postSelectedHandler = (id) => {
@@ -34,13 +38,17 @@ class Blog extends Component {
 
   render () {
 
-      const posts = this.state.posts.map(post =>
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong!!</p>;
+
+    if (!this.state.error) {
+      posts = this.state.posts.map(post =>
         <Post
           key= {post.id}
           title = {post.title}
           author = {post.author}
           clicked = {() => this.postSelectedHandler(post.id)}
         />);
+    }
 
       return (
           <div>
