0297cbda50734bf4353d234b7096a4d6648a2840
chamikab <kasun.chamika@gmail.com>
2018-08-16 00:46:32 +0530

    React rediect support added.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -20,8 +20,11 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost}/>
-          <Route path="/posts" component={Posts}/>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/posts" component={Posts} />
+          {/* from and to only works in Redirect when it is inside the Switch block. */}
+          <Redirect from="/" to="/posts" />
+          {/*<Route path="/" component={Posts}/>*/}
         </Switch>
       </div>
     );
