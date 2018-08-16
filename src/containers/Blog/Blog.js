0f09f82be305b59c0094099352c2e2d76aa3b029
chamikab <kasun.chamika@gmail.com>
2018-08-16 22:45:37 +0530

    Componenet async loading related changes added.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import './Blog.css';
+
+import asyncComponent from '../../hoc/asyncComponent';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 
-import './Blog.css';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -20,7 +25,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          <Route path="/new-post" component={AsyncNewPost} />
           <Route path="/posts" component={Posts} />
           {/* from and to only works in Redirect when it is inside the Switch block. */}
           <Redirect from="/" to="/posts" />
---
+++ src/hoc/asyncComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const asyncComponent = (importedCompoenent) => {
+
+  return class extends Component {
+
+    state = {
+      component: null,
+    };
+
+    componentDidMount () {
+      importedCompoenent()
+        .then(cmp => {
+          this.setState({ component: cmp.default });
+        })
+    }
+
+    render() {
+      const C = this.state.component;
+
+      return C ? <C {...this.props} /> : null;
+
+    };
+  }
+};
+
+export default asyncComponent;
